1b47250189b7b91d9e64848ad563d65b3c4dfdf6
Logan Wood <lwood3499@gmail.com>
2019-06-29 00:38:06 -0500

    wiring up json content to vue component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
 	<div>
 		<h1>This is an about page</h1>
+		<h3>{{phrase}}</h3>
+		<h3>title: {{teks_7_8.title}}</h3>
 	</div>
 </template>
+
+<script>
+	import teks_7_8B from "../content/teks/teks_7_8B.json";
+	console.log(teks_7_8B[0]);
+
+	export default {
+		data() {
+			return {
+				phrase: "This is a message.",
+				teks_7_8: teks_7_8B[0]
+			};
+		}
+	};
+</script>
